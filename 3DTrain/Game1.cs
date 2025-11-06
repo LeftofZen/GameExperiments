89dc1bed716c430d2e4ec5c5f33d910e65b19314
Benjamin Sutas <benjamin.sutas@gmail.com>
2025-11-06 16:16:09 +1100

    add 3dtrain

--- 3DTrain/Game1.cs
+++ 3DTrain/Game1.cs
@@ -5,46 +5,52 @@ using Shared;
 
 namespace _3DTrain
 {
-    public class Game1 : ImGuiGame
-    {
-        Camera3D _camera;
+	public class Game1 : ImGuiGame
+	{
+		Camera3D _camera;
+		Texture2D _tileset;
 
-        public Game1()
-        {
-            Graphics.GraphicsProfile = GraphicsProfile.HiDef;
-            Content.RootDirectory = "Content";
-            IsMouseVisible = true;
-        }
+		public Game1()
+		{
+			Graphics.GraphicsProfile = GraphicsProfile.HiDef;
+			Content.RootDirectory = "Content";
+			IsMouseVisible = true;
+		}
 
-        protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-            _camera = new Camera3D(GraphicsDevice, new Vector3(256, 256, 256));
-            base.Initialize();
-        }
+		protected override void Initialize()
+		{
+			// TODO: Add your initialization logic here
+			_camera = new Camera3D(GraphicsDevice, new Vector3(256, 256, 256));
+			base.Initialize();
+		}
 
-        protected override void LoadContent()
-        {
-            // TODO: use this.Content to load your game content here
-        }
+		protected override void LoadContent()
+		{
+			// TODO: use this.Content to load your game content here
+			_tileset = Content.Load<Texture2D>("Textures//iso_tileset");
+		}
 
-        protected override void Update(GameTime gameTime)
-        {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
-                Exit();
+		protected override void Update(GameTime gameTime)
+		{
+			if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+			{
+				Exit();
+			}
 
-            // TODO: Add your update logic here
+			// TODO: Add your update logic here
 
-            base.Update(gameTime);
-        }
+			base.Update(gameTime);
+		}
 
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+		protected override void Draw(GameTime gameTime)
+		{
+			GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+			SpriteBatch.Begin();
+			SpriteBatch.Draw(_tileset, new Vector2(0, 0), Color.White);
+			SpriteBatch.End();
 
-            base.Draw(gameTime);
-        }
-    }
+			base.Draw(gameTime);
+		}
+	}
 }
